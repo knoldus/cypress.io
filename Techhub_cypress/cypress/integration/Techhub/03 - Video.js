f928756db9b2ed73444eb4f8036ed51b3fca41d9
HimanshuGupta999 <himanshu.gupta@gail.com>
2022-05-24 12:21:07 +0530

    removed duplicate folder

--- Techhub_cypress/cypress/integration/Techhub/03 - Video.js
+++
@@ -1,9 +0,0 @@
-///<reference types = "cypress"/> 
-describe("Google search",function(){
-    it("perform cypress video on demo site",function(){
-
-      cy.visit('https://www.google.co.in/')
-      cy.get('input[title=Search').type('Testing{enter}')
-      cy.get('h3[class="LC20lb MBeuO DKV0Md"]').should('contain','Wikipedia')
-    })
-  })
